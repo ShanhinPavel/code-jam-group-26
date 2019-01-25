c796b78db84f069a7029246916b786cb5849a13d
SystemGhost <jackie07364538@gmail.com>
2019-01-25 20:11:28 +0300

    feat: compile project with images

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,8 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // eslint-disable-next-line import/no-unresolved
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   entry: {
     page1: './src/page1/src/main.js',
@@ -16,7 +18,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'apps/[name]/build/[name].js',
+    filename: 'apps/[name]/[name].js',
     chunkFilename: '[id].[chunkhash].js',
   },
   module: {
@@ -38,31 +40,31 @@ module.exports = {
     new HtmlWebpackPlugin({
       inject: true,
       chunks: ['page1'],
-      filename: 'apps/page1/build/index.html',
+      filename: 'apps/page1/index.html',
       template: '!!html-webpack-plugin/lib/loader.js!./src/template/author.html',
     }),
     new HtmlWebpackPlugin({
       inject: true,
       chunks: ['page2'],
-      filename: 'apps/page2/build/index.html',
+      filename: 'apps/page2/index.html',
       template: '!!html-webpack-plugin/lib/loader.js!./src/template/author.html',
     }),
     new HtmlWebpackPlugin({
       inject: true,
       chunks: ['page3'],
-      filename: 'apps/page3/build/index.html',
+      filename: 'apps/page3/index.html',
       template: '!!html-webpack-plugin/lib/loader.js!./src/template/author.html',
     }),
     new HtmlWebpackPlugin({
       inject: true,
       chunks: ['page4'],
-      filename: 'apps/page4/build/index.html',
+      filename: 'apps/page4/index.html',
       template: '!!html-webpack-plugin/lib/loader.js!./src/template/author.html',
     }),
     new HtmlWebpackPlugin({
       inject: true,
       chunks: ['page5'],
-      filename: 'apps/page5/build/index.html',
+      filename: 'apps/page5/index.html',
       template: '!!html-webpack-plugin/lib/loader.js!./src/template/author.html',
     }),
     new HtmlWebpackPlugin({
@@ -72,7 +74,39 @@ module.exports = {
       template: '!!html-webpack-plugin/lib/loader.js!./src/template/index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: 'apps/[name]/build/[name].css',
+      filename: 'apps/[name]/[name].css',
     }),
+    new CopyWebpackPlugin([
+      {
+        from: './src/page1/src/img',
+        to: 'apps/page1/img',
+        test: /\.(png|jpg|gif|jpeg)$/,
+      },
+      {
+        from: './src/page2/src/img',
+        to: 'apps/page2/img',
+        test: /\.(png|jpg|gif|jpeg)$/,
+      },
+      {
+        from: './src/page3/src/img',
+        to: 'apps/page3/img',
+        test: /\.(png|jpg|gif|jpeg)$/,
+      },
+      {
+        from: './src/page4/src/img',
+        to: 'apps/page4/img',
+        test: /\.(png|jpg|gif|jpeg)$/,
+      },
+      {
+        from: './src/page5/src/img',
+        to: 'apps/page5/img',
+        test: /\.(png|jpg|gif|jpeg)$/,
+      },
+      {
+        from: './src/index/src/img',
+        to: 'apps/index/img',
+        test: /\.(png|jpg|gif|jpeg)$/,
+      },
+    ]),
   ],
 };

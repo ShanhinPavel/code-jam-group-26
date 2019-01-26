00bb2dce8d205b4dd2904de8c57fb82ec5420f6e
SystemGhost <jackie07364538@gmail.com>
2019-01-26 10:34:40 +0300

    feat: add localization function

--- src/list/src/main.js
+++ src/list/src/main.js
@@ -1,5 +1,55 @@
 import data from './data';
 import '../../template/list.css';
 
-console.log('hello, Page1', data.name);
-console.log('hello, Page1', data.author[0]);
+function saveLanguage(lang) {
+  const serialObj = JSON.stringify(lang);
+  localStorage.setItem('Culture-Portal', serialObj);
+}
+
+function changeSite(lang) {
+  let local;
+  Object.keys(data).forEach((key) => {
+    if (key === lang) {
+      local = data[key];
+    }
+  });
+  let temp;
+  Object.keys(local).forEach((key) => {
+    temp = document.getElementById(key);
+    temp.innerHTML = local[key];
+  });
+}
+
+function changeLang(lang) {
+  if (lang !== 'ru' && lang !== 'en' && lang !== 'by') {
+    saveLanguage('ru');
+    return;
+  }
+  const languageSite = JSON.parse(localStorage.getItem('Culture-Portal'));
+  if (languageSite !== lang) {
+    saveLanguage(lang);
+    changeSite(lang);
+  }
+}
+
+function checkLanguage() {
+  let languageSite = JSON.parse(localStorage.getItem('Culture-Portal'));
+  if (languageSite === null) {
+    saveLanguage('ru');
+    languageSite = 'ru';
+  }
+  return languageSite;
+}
+
+function init() {
+  const startLang = checkLanguage();
+  changeSite(startLang);
+  document.getElementsByClassName('lang__button')[0].addEventListener('click', (event) => {
+    event.preventDefault();
+    changeLang(event.target.dataset.lang);
+  });
+}
+
+window.onload = () => {
+  init();
+};

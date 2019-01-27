633ea97a3b1723e5593ae6000adef0f6575b02a3
SystemGhost <jackie07364538@gmail.com>
2019-01-27 15:37:59 +0300

    feat: add translate to index.html

--- src/index/src/main.js
+++ src/index/src/main.js
@@ -1,5 +1,6 @@
 import '../../template/index.css';
 import shortInformation from './shortInformation.json';
+import dataTranslate from './data';
 
 function getAuthor(data) {
   // Get authors by language
@@ -15,6 +16,7 @@ function getAuthor(data) {
     day = 4;
   }
   author = data.authorsList[day];
+  console.log('author', author);
   // Get author from json
   const authorData = authorCollectionByLanguage[author];
   return authorData;
@@ -30,9 +32,58 @@ function addAuthorToPage(authorData, id) {
   authorDescription.textContent = authorData.description;
 }
 
+function saveLanguage(lang) {
+  const serialObj = JSON.stringify(lang);
+  localStorage.setItem('Culture-Portal', serialObj);
+}
+
+function changeSite(lang) {
+  let local;
+  Object.keys(dataTranslate).forEach((key) => {
+    if (key === lang) {
+      local = dataTranslate[key];
+    }
+  });
+  let temp;
+  Object.keys(local).forEach((key) => {
+    temp = document.getElementById(key);
+    temp.innerHTML = local[key];
+  });
+}
+
+function changeLang(lang) {
+  if (lang !== 'ru' && lang !== 'en' && lang !== 'by') {
+    saveLanguage('ru');
+    return;
+  }
+  const languageSite = JSON.parse(localStorage.getItem('Culture-Portal'));
+  if (languageSite !== lang) {
+    saveLanguage(lang);
+    changeSite(lang);
+  }
+}
+
+function checkLanguage() {
+  let languageSite = JSON.parse(localStorage.getItem('Culture-Portal'));
+  if (languageSite === null) {
+    saveLanguage('ru');
+    languageSite = 'ru';
+  }
+  return languageSite;
+}
+
+function init() {
+  const startLang = checkLanguage();
+  changeSite(startLang);
+  document.getElementsByClassName('main__lang-buttons')[0].addEventListener('click', (event) => {
+    event.preventDefault();
+    changeLang(event.target.dataset.lang);
+  });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+  init();
   const elementId = 'day-author';
   const author = getAuthor(shortInformation);
-  console.log(author);
   addAuthorToPage(author, elementId);
 });
